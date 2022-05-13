998ec0bf2060dc9ce17fb402bdb8522870166263
officialksolomon <solomonuche42@gmail.com>
2022-05-13 10:36:57 +0100

    added contact page ui design

--- main/models.py
+++ main/models.py
@@ -3,8 +3,6 @@ from email import message
 from django.db import models
 
 # Create your models here.
-
-
 class Contact(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from main.views import ContactView
+
+app_name = 'main'
 urlpatterns = [
     path('contact/', ContactView.as_view(), name='contact'),
 ]
