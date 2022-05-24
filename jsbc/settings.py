b5421a3562ee0a85f4a6ef25efaf0ddce45412cc
officialksolomon <solomonuche42@gmail.com>
2022-05-24 22:53:13 +0100

    reverted change

--- jsbc/settings.py
+++ jsbc/settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import environ
-#
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uhkwistvvopr_-(-ame))kirz=td&^5dh7$12r*t*l&rx0y@yc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -45,7 +42,7 @@ INSTALLED_APPS = [
     # 'crispy_forms'
     'crispy_forms',
     # livereload
-
+    'livereload',
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -171,7 +168,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+#
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Raises django's ImproperlyConfigured exception if PAYSTACK_SECRET_KEY not in os.environ
 PAYSTACK_SECRET_KEY = env("PAYSTACK_SECRET_KEY")
