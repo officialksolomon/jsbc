bb54bffca299cc014d224720b11cf29d4355f4ad
officialksolomon <solomonuche42@gmail.com>
2022-06-01 13:34:21 +0100

    email settings update in settings.py

--- jsbc/settings.py
+++ jsbc/settings.py
@@ -198,7 +198,7 @@ MEDIA_URL = '/media/'
 # Bottom of the file
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
